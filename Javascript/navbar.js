50fd0d2dc1f45e0afc5637ff6d0d691153e91228
isweat-exe <isweatmc@gmail.com>
2024-05-04 10:59:50 +0200

    Fin Dashboard version PC (1/4)

---
+++ Javascript/navbar.js
@@ -0,0 +1,7 @@
+function SidebarClose() {
+    document.querySelector(".sidebar").style.transform = "translateX(-100%)";
+}
+
+function NavbarOpenMobile() {
+    document.querySelector(".sidebar").style.transform = "translateX(0%)";
+}
